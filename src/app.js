3e8aab053277dbb3759d0c132eb3aa6a55265769
Alexander Morales <alexandermorales.n1@gmail.com>
2025-03-05 10:28:02 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let htmlTxt = document.querySelector('#excuse')
+const randomNum2 = function(){
+  let num = Math.floor(Math.random() * 2) + 1
+  return num;
+}
+const randomNum3 = function(){
+  let num = Math.floor(Math.random() * 3) + 1
+  return num;
+}
+const randomNum4 = function(){
+  let num = Math.floor(Math.random() * 4) + 1
+  return num;
+}
+htmlTxt.innerHTML = `${who[randomNum3()]} ${action[randomNum3()]} ${what[randomNum2()]} ${when[randomNum4()]}`
 };
